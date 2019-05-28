6573756cded940403cb244a70f5249c07caa518a
Thiago Scolari (Application Management Group) <thiago.scolari@wipro.com>
2019-05-28 11:30:56 +0100

    config commit

---
+++ src/main/java/com/thiago/pontointeligente/api/entities/Funcionario.java
@@ -0,0 +1,5 @@
+package com.thiago.pontointeligente.api.entities;
+
+public class Funcionario {
+
+}
---
+++ src/main/java/com/thiago/pontointeligente/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.thiago.pontointeligente.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
